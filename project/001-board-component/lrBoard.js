ef9fca1f7fac820530f6a5dfb1f669b5647813b5
lurui <299294538@qq.com>
2019-04-26 16:56:13 +0800

    component testing 50%

---
+++ project/001-board-component/lrBoard.js
@@ -0,0 +1,79 @@
+;
+(function() {
+    'use strict'
+    window.lrBoard = {
+        init
+    };
+    // 全局变量,方便调用
+    let board, inner, list, count, config;
+    // 默认配置
+    let defaultConfig = {
+            list: ['test1', 'test2', 'test3'],
+            interval: 500,
+        }
+        /*************init一切的开始********************/
+        /**
+         * 广告牌制作
+         * @param {string} boardSelector  广告容器
+         * @param {string} innerSelector  广告容器子级
+         * @param {Object} adlist   广告内容
+         */
+    function init(boardSelector, innerSelector, adlist) {
+        //选中广告容器
+        board = document.querySelector(boardSelector);
+        //选中广告容器子级
+        inner = board.querySelector(innerSelector);
+        // 加载用户配置
+        loadConfig(adlist);
+        //广告索引默认从0开始
+        count = 0;
+        //广告内容从头开始
+        inner.innerHTML = config.list[count];
+        // 广告闪烁开始表演
+        setadFlash();
+    }
+    /**
+     * 加载用户配置
+     * @param {Object} adlist 用户配置
+     */
+    function loadConfig(adlist) {
+        config = Object.assign({}, defaultConfig, adlist)
+        console.log("+++++", config)
+    };
+    /**
+     * 广告闪烁,实质为定时
+     */
+    function setadFlash() {
+        setInterval(() => {
+            /*切换广告内容时,样式改变,运用了回调函数
+            函数调用的是匿名函数
+            */
+            toggleText(
+                () => {
+                    board.style.borderColor = 'transparent';
+                },
+                () => {
+                    board.style.borderColor = 'inherit';
+                    count++;
+                    if (count > config.list.length - 1) {
+                        count = 0;
+                    }
+                    inner.innerText = config.list[count];
+                })
+        }, config.interval);
+    };
+    // 判断广告内容是否为显示状态
+    function visible() {
+        return getComputedStyle(inner).opacity == '1'
+    }
+    // 切换广告内容
+    function toggleText(hide, show) {
+        if (visible()) {
+            hide();
+            inner.style.opacity = 0;
+        } else {
+            show();
+            inner.style.opacity = 1;
+        }
+    }
+})();
