ea23d9aa9d19555db85d487072944a6badc1f475
lurui <299294538@qq.com>
2019-09-17 11:25:19 +0800

    vue-Todo 50%

--- project/zvue-Todo/js/main.js
+++ project/zvue-Todo/js/main.js
@@ -1,47 +1,119 @@
 ;
 (function() {
     'use strict'
-
+    /**
+     * 合并对象
+     * @param {Object}} obj 
+     */
     function copy(obj) {
         return Object.assign({}, obj)
     }
+    var alert_sound = document.getElementById('alert-sound')
     var app = new Vue({
         el: '#main',
         data() {
             return {
                 list: [],
                 last_id: 0,
-                current: {}
+                current: {},
             }
         },
         mounted() {
             this.list = boot.get('list') || this.list;
             this.last_id = boot.get('last_id') || this.last_id;
+            var time = setInterval(() => {
+                app.check_alerts();
+            }, 1000);
         },
         methods: {
-            merge() {
-                var title = this.current.title;
-                if (!title && title !== 0) return;
-                var todo = copy(this.current);
-                this.last_id++;
-                boot.set('last_id', this.last_id);
-                todo.id = this.last_id;
-                this.list.push(todo);
-                console.log(this.list)
-                this.reset_current()
+            /**
+             * 是否提示
+             */
+            check_alerts() {
+                this.list.forEach((item, i) => {
+                    console.log(item, i)
+                    if (!item.alert_at || item.alert_confirmed) return;
+                    var alert_at = (new Date(item.alert_at)).getTime();
+                    var now = (new Date()).getTime();
+                    if (now >= alert_at) {
+                        alert_sound.play();
+                        var confirmed = confirm(item.title);
+                        Vue.set(this.list[i], 'alert_confirmed', confirmed);
+                    }
+                });
             },
+            /**
+             * 是否完成
+             */
+            toggle_complete(id) {
+                var index = this.find_index(id);
+                Vue.set(this.list[index], 'completed', !this.list[index].completed);
+            },
+            /**
+             * todo列表信息
+             */
+            merge() {
+                var is_update, id;
+                is_update = id = this.current.id;
+                if (is_update) {
+                    var index = this.find_index(id)
+                    Vue.set(this.list, index, copy(this.current))
+                } else {
+                    var title = this.current.title;
+                    if (!title && title !== 0) return;
+                    var todo = copy(this.current);
+                    this.last_id++;
+                    boot.set('last_id', this.last_id);
+                    todo.id = this.last_id;
+                    this.list.push(todo);
+                }
+                this.reset_current();
+            },
+            /**
+             * 清空输入框数据
+             */
             reset_current() {
                 this.set_current({});
             },
+            /**
+             * 更新current对象
+             * @param {Object} todo 
+             */
             set_current(todo) {
                 this.current = copy(todo)
+            },
+            /**
+             * 删除数据
+             */
+            remove(id) {
+                var index = this.find_index(id)
+                this.list.splice(index, 1)
+            },
+            /**
+             * 根据id查找数组index
+             */
+            find_index(id) {
+                return this.list.findIndex(item => {
+                    return item.id == id
+                })
+            },
+            /**
+             * 详细信息
+             */
+            toggle_detail(id) {
+                var index = this.find_index(id)
+                Vue.set(this.list[index], 'show_detail', !this.list[index].show_detail)
             }
         },
+        /**
+         * 监听list,有变化则存到localstorge里
+         */
         watch: {
             list: {
                 deep: true,
                 handler(newval, oldval) {
                     if (newval) {
+                        console.log(newval)
                         boot.set('list', newval);
                     } else {
                         boot.set('list', []);
