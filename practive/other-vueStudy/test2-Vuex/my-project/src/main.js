bdf80b407cd4776f03c34f42f5967c692639155a
xxxlrlovegd <Lruixx@163.com>
2020-07-23 15:49:44 +0800

    vue-router otherInput*-404

--- practive/other-vueStudy/test2-Vuex/my-project/src/main.js
+++ practive/other-vueStudy/test2-Vuex/my-project/src/main.js
@@ -6,6 +6,7 @@ import 'view-design/dist/styles/iview.css';
 import store from './vuex/store.js';
 import Good from '../src/components/Goods';
 import GoodNew from '../src/components/GoodNew';
+import Errors from '../src/components/Error';
 Vue.config.productionTip = false
 Vue.use(VueRouter);
 Vue.use(ViewUI);
@@ -17,7 +18,10 @@ const RouterConfig = [{
   path: '/GoodNew',
   name: "订单详情",
   component: GoodNew,
-}, ]
+}, {
+  path: '*',
+  component: Errors,
+}]
 new Vue({
   store,
   render: h => h(App),
