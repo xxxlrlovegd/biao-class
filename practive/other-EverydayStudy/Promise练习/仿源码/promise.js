b2cfe0fedd346e9618cc1dd13c24ac122a0345b4
xxxlgloverd <lruixx@163.com>
2021-12-22 17:05:54 +0800

    promise test

--- practive/other-EverydayStudy/Promise练习/仿源码/promise.js
+++ practive/other-EverydayStudy/Promise练习/仿源码/promise.js
@@ -13,8 +13,10 @@ function Promise(executor) {
     self.PromiseState = "fulfilled";
     self.PromiseResult = data;
     //执行回调函数
-    self.callbacks.forEach(function (element) {
-      element.onResolved(data);
+    setTimeout(function () {
+      self.callbacks.forEach((element) => {
+        element.onResolved(data);
+      });
     });
   }
   //reject函数
@@ -23,8 +25,10 @@ function Promise(executor) {
     self.PromiseState = "rejected";
     self.PromiseResult = data;
     //执行回调函数
-    self.callbacks.forEach(function (element) {
-      element.onRejected(data);
+    setTimeout(function () {
+      self.callbacks.forEach(function (element) {
+        element.onRejected(data);
+      });
     });
   }
   //用try catch 对 throw错误进行处理
@@ -36,41 +40,114 @@ function Promise(executor) {
   }
 }
 Promise.prototype.then = function (onResolved, onRejected) {
+  if (typeof onResolved !== "function") {
+    onResolved = (value) => {
+      value;
+    };
+  }
+  if (typeof onRejected !== "function") {
+    onRejected = (reason) => {
+      throw reason;
+    };
+  }
   return new Promise((resolve, reject) => {
     //调用回调函数  PromiseState
-    //  if(this.PromiseState === 'fulfilled'){ onResolved(this.PromiseResult);} 未修改时代码
-    let result = onResolved(this.PromiseResult);
-    console.log(result);
-    if (this.PromiseState === "fulfilled") {
+    function callback(type) {
       try {
-        //获取回调函数的执行结果
-        let result = onResolved(this.PromiseResult);
-        console.log(result);
-        //判断
+        let result = type(this.PromiseResult);
         if (result instanceof Promise) {
-          //如果是 Promise 类型的对象,我就将下一个promise结果赋予外层
           result.then(
-            (v) => {
-              resolve(v);
-            },
-            (r) => {
-              reject(r);
-            }
+            (v) => this.resolve(v),
+            (e) => this.reject(e)
           );
         } else {
           resolve(result);
-        } //如果返回的不是promise对象,都将其赋予成功状态
-      } catch (e) {
-        alert(e);
-        rejerect(e); //如果出错了,则返回失败状态
+        }
+      } catch (error) {
+        reject(error);
       }
     }
-    if (this.PromiseState === "rejected") {
-      onRejected(this.PromiseResult);
+    if (this.PromiseState === "fulfilled") {
+      setTimeout(function () {
+        callback(onResolved);
+      }, 0);
     }
+    if (this.PromiseState === "rejected") {
+      setTimeout(function () {
+        callback(onRejected);
+      }, 0);
+    }
+
     //判断 pending 状态
     if (this.PromiseState === "pending") {
-      this.callbacks.push({ onResolved: onResolved, onRejected: onRejected });
+      this.callbacks.push({
+        onResolved: () => {
+          callback(onResolved);
+        },
+        onRejected: function () {
+          callback(onRejected);
+        },
+      });
+    }
+  });
+};
+
+Promise.prototype.catch = function () {
+  return this.then(undefined, onRejected);
+};
+
+Promise.resolve = function (value) {
+  return new Promise((resolve, reject) => {
+    if (value instanceof Promise) {
+      value.then(
+        (v) => resolve(v),
+        (e) => reject(e)
+      );
+    } else {
+      resolve(value);
+    }
+  });
+};
+
+Promise.reject = function (reason) {
+  return new Promise((resolve, reject) => {
+    reject(reason);
+  });
+};
+
+Promise.all = function (promise) {
+  return new Promise((resolve, reject) => {
+    const arr = [];
+    let count = 0;
+    let len = promise.length;
+    for (let i = 0; i < len; i++) {
+      promise[i].then(
+        (v) => {
+          arr[i] = v;
+          if (++count == len) {
+            resolve(v);
+          }
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+    }
+  });
+};
+
+Promise.race = function (promise) {
+  return new Promise((resolve, reject) => {
+    let len = promise.length;
+    for (let i = 0; i < len; i++) {
+      promise[i].then(
+        (v) => {
+          resolve(v);
+        },
+        (e) => {
+          reject(e);
+        }
+      );
     }
   });
 };
